74556ec7a96bab1e6f51ac88f71efee2432f82ad
Ziqian Zeng <ziqianzeng@outlook.com>
2022-06-13 17:02:16 +0800

    Update uci_bert.py

--- uci_bert.py
+++ uci_bert.py
@@ -6,7 +6,7 @@ parser = argparse.ArgumentParser(description='VWS')
 
 parser.add_argument('--unsupervised', type=bool, default=True,
                     help='whether train model unsupervisedly')
-parser.add_argument('--lm_name', type=str, default="bert-base-cased",
+parser.add_argument('--lm_name', type=str, default="bert-base-uncased",
                     help='pre-trained language model')
 
 parser.add_argument('--train', type=str, default="train",
